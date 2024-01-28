f4eed3a1db0430e3a9476743db30d6722fc20adc
Christian Hernandez <cristian.28hedz@gmail.com>
2024-01-28 12:08:53 -0600

    initial commit

---
+++ src/main/java/org/chris/myfirstapirest/api/MyFirstApiRestSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.chris.myfirstapirest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstApiRestSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstApiRestSpringbootApplication.class, args);
+    }
+
+}
