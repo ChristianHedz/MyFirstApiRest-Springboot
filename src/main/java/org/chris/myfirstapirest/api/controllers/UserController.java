536f68b978701ecb1b4ebe8aadc1440fbc038bb6
Christian Hernandez <cristian.28hedz@gmail.com>
2024-01-28 23:27:40 -0600

    feat: 🦺 Se agrego validacion para los campos del usuario

---
+++ src/main/java/org/chris/myfirstapirest/api/controllers/UserController.java
@@ -0,0 +1,49 @@
+package org.chris.myfirstapirest.api.controllers;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.chris.myfirstapirest.api.models.dto.UserDto;
+import org.chris.myfirstapirest.api.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/v1")
+public class UserController {
+
+    private UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        List<UserDto> userDtoList = userService.listAllUsers();
+        return new ResponseEntity<>(userDtoList, HttpStatus.OK);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<UserDto> createUser(@RequestBody @Valid UserDto userDTO){
+        UserDto userDto = userService.saveUser(userDTO);
+        return new ResponseEntity<>(userDto,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long id,@RequestBody @Valid UserDto userDTO){
+        UserDto userDto = userService.updateUser(id,userDTO);
+        return new ResponseEntity<>(userDto,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserDto> findUserById(@PathVariable Long id){
+        UserDto userDto = userService.findUserById(id);
+        return new ResponseEntity<>(userDto,HttpStatus.OK);
+    }
+}
--- src/main/java/org/chris/myfirstapirest/api/models/dto/UserDto.java
+++ src/main/java/org/chris/myfirstapirest/api/models/dto/UserDto.java
@@ -1,10 +1,22 @@
 package org.chris.myfirstapirest.api.models.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserDto {
+
+    @NotBlank(message = "{user.name.notBlank}")
     private String name;
+
+    @NotBlank(message = "{user.lastname.notBlank}")
     private String lastname;
+
+    @NotBlank(message = "{user.email.notBlank}")
+    @Email(message = "{user.email.format}")
     private String email;
+
 }
