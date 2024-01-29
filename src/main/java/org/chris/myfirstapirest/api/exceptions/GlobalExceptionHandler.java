a3230697fe72f196da27898ab9995187c4887731
Christian Hernandez <cristian.28hedz@gmail.com>
2024-01-28 23:30:17 -0600

    feat: 🥅 Se agrego excepciones NullPointerException, HttpMessageNotReadableException y MethodArgumentNotValidException al GlobalExceptionHandler

--- src/main/java/org/chris/myfirstapirest/api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/chris/myfirstapirest/api/exceptions/GlobalExceptionHandler.java
@@ -1,11 +1,17 @@
 package org.chris.myfirstapirest.api.exceptions;
 
 import org.chris.myfirstapirest.api.payload.ApiResponse;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.List;
 
 
 @RestControllerAdvice
@@ -16,4 +22,26 @@ public class GlobalExceptionHandler {
         ApiResponse response = new ApiResponse(ex.getMessage(), HttpStatus.NOT_FOUND, webRequest.getDescription(false));
         return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<ApiResponse> handleNullPointerException(NullPointerException ex, WebRequest webRequest){
+        ApiResponse response = new ApiResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, webRequest.getDescription(false));
+        return new ResponseEntity<>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex, WebRequest webRequest){
+        ApiResponse response = new ApiResponse(ex.getMessage(), HttpStatus.BAD_REQUEST, webRequest.getDescription(false));
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ProblemDetail> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,WebRequest webRequest){
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        List<String> errors = ex.getBindingResult().getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage).toList();
+        problemDetail.setProperty("errors",errors);
+        return new ResponseEntity<>(problemDetail,HttpStatus.BAD_REQUEST);
+    }
+
 }
