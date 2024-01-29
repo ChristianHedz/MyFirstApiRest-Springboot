a7cd4fc6a486d869abcfb5f8a45081ec313cc77f
Christian Hernandez <cristian.28hedz@gmail.com>
2024-01-28 22:40:35 -0600

    feat: ✨ se agrego un bean que retorne instancias de UserMapper y se agrego la dependencia de mapstruct processor

--- src/main/java/org/chris/myfirstapirest/api/MyFirstApiRestSpringbootApplication.java
+++ src/main/java/org/chris/myfirstapirest/api/MyFirstApiRestSpringbootApplication.java
@@ -1,13 +1,21 @@
 package org.chris.myfirstapirest.api;
 
+import org.chris.myfirstapirest.api.mapper.UserMapper;
+import org.mapstruct.factory.Mappers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyFirstApiRestSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyFirstApiRestSpringbootApplication.class, args);
+    }
+
+    @Bean
+    public UserMapper userMapper(){
+        return Mappers.getMapper(UserMapper.class);
     }
 
 }
